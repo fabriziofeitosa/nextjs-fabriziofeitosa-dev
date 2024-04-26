a927ee1554124debc32721b0e7a4a7ddd88cb018
Fabrizio Feitosa <fabriziofeitosa@live.com>
2024-04-26 09:03:16 -0300

    Recomeçando projeto

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Fabrizio Feitosa - Blog",
+  url: "https://fabriziofeitosa.dev",
+  description: "Meu blog pessoal, onde compartilho conhecimento sobre desenvolvimento web e tecnologia em geral.",
+  author: "Fabrizio Feitosa",
+  links: {
+    twitter: "https://twitter.com/fabriziofeitosa",
+    github: "https://github.com/fabriziofeitosa",
+    personalSite: "https://fabriziofeitosa.dev",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
