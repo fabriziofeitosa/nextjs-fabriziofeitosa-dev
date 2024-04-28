b5aa2fea5ccea761674d1e3e6a28cbe50d1970a3
Fabrizio Feitosa <fabriziofeitosa@live.com>
2024-04-28 01:57:06 -0300

    sticky header

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -11,59 +11,61 @@ interface SiteHeaderProps {
 export function SiteHeader({ className }: SiteHeaderProps) {
   return (
     <header className={className}>
-      <Image
-        src="/photo.png"
-        alt="Fabrizio Feitosa"
-        width={75}
-        height={75}
-        className="rounded-full"
-      />
-      <h1 className="my-3 text-2xl font-mono">{siteConfig.name}</h1>
-      <p className="mt-3 mb-0 text-muted-foreground">
-        {siteConfig.description}
-      </p>
-      <nav className="my-5">
-        <MainNav />
-      </nav>
-      <div className="social mt-8">
-        <ul className="flex flex-row gap-2">
-          <li>
-            <Link
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <Icons.twitter className="w-6 h-6" />
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <Icons.gitHub className="w-6 h-6" />
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={siteConfig.links.linkedin}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <Icons.linkedin className="w-6 h-6" />
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={siteConfig.links.instagram}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <Icons.instagram className="w-6 h-6" />
-            </Link>
-          </li>
-        </ul>
+      <div className="content md:sticky md:top-8">
+        <Image
+          src="/photo.png"
+          alt="Fabrizio Feitosa"
+          width={75}
+          height={75}
+          className="rounded-full"
+        />
+        <h1 className="my-3 text-2xl font-mono">{siteConfig.name}</h1>
+        <p className="mt-3 mb-0 text-muted-foreground">
+          {siteConfig.description}
+        </p>
+        <nav className="my-5">
+          <MainNav />
+        </nav>
+        <div className="social mt-8">
+          <ul className="flex flex-row gap-2">
+            <li>
+              <Link
+                href={siteConfig.links.twitter}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Icons.twitter className="w-6 h-6" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={siteConfig.links.github}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Icons.gitHub className="w-6 h-6" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={siteConfig.links.linkedin}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Icons.linkedin className="w-6 h-6" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={siteConfig.links.instagram}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Icons.instagram className="w-6 h-6" />
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </header>
   );
